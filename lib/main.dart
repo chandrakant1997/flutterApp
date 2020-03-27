6f122fe8e360809475c3b8b3bd33f5af56800173
chandrakant1997 <ckpatel802@gmail.com>
2020-03-27 15:17:37 +0530

    removed space

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:airticket_booking/ticketbook.dart';
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(MaterialApp(
-
-    title: "flutter app",
-    home: Ticket(),
-    debugShowCheckedModeBanner: false,
-  ),
+void main() {
+  runApp(
+    MaterialApp(
+      title: "flutter app",
+      home: Ticket(),
+      debugShowCheckedModeBanner: false,
+    ),
   );
-  }
-
+}
